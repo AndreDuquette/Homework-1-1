8ae234395dedde3114fbced3a7346b4b4e475d9e
AndreDuquette <75873798+AndreDuquette@users.noreply.github.com>
2021-01-23 11:19:07 -0500

    aaa
    aaaaaa

--- Functions.swift
+++ Functions.swift
@@ -1,6 +1,6 @@
-//Name : 
-//Start Date :
-//Due Date : 
+//Name : Andre Duquette
+//Start Date : 1/23/21
+//Due Date : 1/30/21
 
 //Function practice
 /* Your job is to create 10 separate actions needed to prepare for school in the morning.
@@ -15,3 +15,13 @@ Call each of your functions and run your code.
  }
 
  wakeUp()       // This will print "Wake up at 7 am"
+
+//Work
+
+
+func shower(){
+ print("Get in the shower and take a 5 - 10 minute shower")
+}
+func dressUp(){
+ print ("Put on some appropriate clothing")
+}
